3c7f119c062a2db9ed1a17a161dc3352621ed455
richardlee-kr <richardl22245@gmail.com>
2021-06-04 16:29:06 +0900

    Notification alert

---
+++ app/src/main/java/com/example/floatingwidget/ApplicationClass.java
@@ -0,0 +1,11 @@
+package com.example.floatingwidget;
+
+import android.app.Application;
+import android.content.Context;
+
+public class ApplicationClass  extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/example/floatingwidget/MainActivity.java
+++ app/src/main/java/com/example/floatingwidget/MainActivity.java
@@ -3,18 +3,25 @@ package com.example.floatingwidget;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Build;
 import android.provider.Settings;
+import android.util.Log;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
     Spinner character_select; //dropdown menu
 
     Button addButtonWidget; //Button to Add Widget
+    ImageButton play;
+    TextView title;
+
+    List<Track> tracks;
+
+    NotificationManager notificationManager;
 
     int frameSpeed = 30;
     String character = "Lucifer";
@@ -33,12 +46,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        populateTracks();
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            createChannel();
+        }
+
         frame_bar = (SeekBar) findViewById(R.id.frame_speed);
         frame_text = (TextView) findViewById(R.id.frame_speed_text);
+
+        title = (TextView) findViewById(R.id.title);
 
         character_select = (Spinner) findViewById(R.id.character_list);
 
         addButtonWidget = (Button) findViewById(R.id.button_widget); //find the button in layout
+        play = (ImageButton) findViewById(R.id.play);
 
         frame_bar.incrementProgressBy(1);
 
@@ -78,6 +101,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateNotification.createNotification(MainActivity.this, tracks.get(0), R.drawable.ic_baseline_pause_24, 1, tracks.size() - 1);
+            }
+        });
+
         addButtonWidget.setOnClickListener(new View.OnClickListener() { //Button Click Event
             @Override
             public void onClick(View view) {
@@ -91,8 +121,8 @@ public class MainActivity extends AppCompatActivity {
                         Intent intent = new Intent(MainActivity.this, WidgetService.class); //create intent of WidgetService
                         intent.putExtra("speed", frameSpeed); //frame Speed transmit
                         intent.putExtra("character", character);
-                        startService(intent); //start Widget
-                        finish(); //end MainActivity
+                        //startService(intent); //start Widget
+                        //finish(); //end MainActivity
                     }
                 }
             }
@@ -109,15 +139,36 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1)
+    private void createChannel()
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
         {
-            if(!Settings.canDrawOverlays(this))
+            NotificationChannel channel = new NotificationChannel(CreateNotification.CHANNEL_ID,"richard", NotificationManager.IMPORTANCE_LOW);
+
+            notificationManager = getSystemService(NotificationManager.class);
+            if(notificationManager != null)
             {
-                Toast.makeText(this, "Permssion denied by user.", Toast.LENGTH_SHORT).show();
+                notificationManager.createNotificationChannel(channel);
             }
         }
     }
+
+    private void populateTracks()
+    {
+        tracks = new ArrayList<>();
+
+        tracks.add(new Track("Track 1", "Artist 1", R.drawable.vitality));
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode == 1)
+//        {
+//            if(!Settings.canDrawOverlays(this))
+//            {
+//                Toast.makeText(this, "Permssion denied by user.", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
 }
